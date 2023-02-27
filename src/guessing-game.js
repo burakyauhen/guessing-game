f6e9986980d6ff24d554cfb0ed98ef7cfd85b0c5
Burak Yauhen <burakyauhen@gmail.com>
2023-02-27 13:31:21 +0300

    feat: add code

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min;
+        this.max;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.ceil((this.max + this.min) / 2);
     }
 
     lower() {
-
+        this.max = this.guess();
     }
 
     greater() {
-
+        this.min = this.guess();
     }
 }
+
 
 module.exports = GuessingGame;
